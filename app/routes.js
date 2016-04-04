a8a36a569e0ca835b646dbe001ca031b9147033f
Isaac Hartung <isaac.hartung@gmail.com>
2016-04-04 11:20:34 -0600

    Gossip Lab

---
+++ app/routes.js
@@ -0,0 +1,202 @@
+// app/routes.js
+var fs = require("fs");
+var request = require("request");
+
+
+module.exports = function(app) {
+
+    // =====================================
+    // HOME PAGE (with login links) ========
+    // =====================================
+    app.get('/', function(req, res) {
+        if('user_name' in me) {
+          res.render('profile.ejs',{
+              user_name: me.user_name,
+              list: messages
+          });
+        }
+        else res.render('index.ejs'); // load the index.ejs file
+    });
+
+
+    app.post('/', function(req, res) {
+
+        console.log("post/");
+
+        console.log(req.body);
+
+        if(!("url" in me)){
+
+          var fullUrl = req.protocol + '://' + req.get('host');
+
+          me.url = fullUrl;
+        }
+
+        me.user_name = req.body.username;
+
+        console.log(JSON.stringify(me));
+
+        res.redirect('/');
+
+    });
+
+    //add local message
+    app.post('/local/message', function(req, res){
+        //get all messages
+
+        console.log("post/local/message");
+
+        var messageid = me.uuid + ":" + me.m_count;
+        var message = {"MessageID":messageid, "Originator": me.user_name, "Text": req.body.message}
+        me.want[me.uuid] = me.m_count;
+        me.m_count = me.m_count + 1;
+
+
+
+        messages.messages[messages.count] = message;
+        messages.count = messages.count + 1;
+
+
+        console.log(messages);
+        console.log(me.want);
+
+        res.redirect('/');
+    });
+
+
+
+
+    //add message from other peer
+    app.post('/message', function(req, res){
+        //get all messages
+        if(!("url" in me)){
+
+          var fullUrl = req.protocol + '://' + req.get('host');
+
+          me.url = fullUrl;
+        }
+
+        console.log("post/message");
+
+        console.log(req.body);
+        //get and parse body of request
+        var message = req.body.message;
+
+        console.log(req.body.message);
+
+        if(message.hasOwnProperty("Want")){
+
+          console.log(peers);
+
+          var found = false;
+
+          for(i = 0; i < peers.count; i++){
+            if(peers.peers[i].url == message.Endpoint){
+              peers.peers[i].want = message.Want;
+              found = true;
+            }
+          }
+
+          if(!found){
+            peers.peers[peers.count] = {"want": message.Want, "url":message.Endpoint};
+            peers.count = peers.count + 1;
+          }
+
+        }
+        else if(message.hasOwnProperty("Rumor")){
+          //Rumor
+
+          console.log("inRumor");
+
+          var mid = JSON.stringify(message.Rumor.MessageID).replace(/\"/g, "");
+          var array = mid.split(":");
+          var uuid = array[0].trim();
+          var sn = array[1].trim();
+          var good = false;
+
+          console.log(uuid);
+          console.log(sn);
+          console.log(message.Rumor);
+
+          //if the message uuid is in me
+          if(uuid in me.want){
+            //if I do not have the message
+            if(sn > me.want[uuid]){
+              good = true;
+            }
+          }
+          else good = true;
+
+          console.log(good);
+
+          if(good){
+            messages.messages[messages.count] = message.Rumor;
+            messages.count = messages.count + 1;
+            me.want[uuid] = sn;
+          }
+
+          console.log(messages);
+          console.log(me);
+
+          var found = false;
+
+          for(i = 0; i < peers.count; i++){
+            if(peers.peers[i].url == message.Endpoint){
+              found = true;
+            }
+          }
+
+          if(!found){
+            peers.peers[peers.count] = {"want": {}, "url":message.Endpoint};
+            peers.peers[peers.count].want[uuid] = sn;
+            peers.count = peers.count + 1;
+          }
+
+        } else{
+          console.log("neither");
+
+          console.log(peers);
+
+          var found = false;
+
+          for(i = 0; i < peers.count; i++){
+            if(peers.peers[i].url == message.Endpoint){
+              peers.peers[i].want = {};
+              found = true;
+            }
+          }
+
+          if(!found){
+            peers.peers[peers.count] = {"want": {}, "url":message.Endpoint};
+            peers.count = peers.count + 1;
+          }
+
+        }
+
+
+
+        res.status(200).send('OK');
+
+    });
+
+    //add a peer
+    app.post('/peer', function(req, res){
+        //var u = req.body.peer;
+        console.log("post/peer");
+
+        var url = JSON.stringify(req.body.peer).replace(/\"/g, "");
+        var murl = "http://" + url;
+
+        var peer = { "want":{}, "url" : murl}
+        peers.peers[peers.count] = peer;
+        peers.count = peers.count + 1;
+
+        console.log(peers);
+
+        res.redirect('/');
+
+    });
+
+
+
+};
---
+++ server.js
@@ -0,0 +1,158 @@
+// server.js
+
+// set up ======================================================================
+// get all the tools we need
+var fs = require('fs');
+var express  = require('express');
+var http = require('http')
+var app      = express();
+var request = require('request');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+
+var uuid = require("node-uuid");
+
+var myuuid = uuid.v1();
+
+global.me = { "m_count":0,"uuid": myuuid , "want":{} };
+global.messages = {"count":0, "messages":[]}
+global.peers = {"count":0, "peers":[]};
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+
+// routes ======================================================================
+require('./app/routes.js')(app);
+
+// launch ======================================================================
+//app.listen(port);
+http.createServer(app).listen(80);
+console.log('The magic happens on port ' + 80);
+loop();
+
+
+function compareWants(otherWant, index){
+  console.log("incw");
+	var myWant = me.want;
+	var mykeys = Object.keys(myWant);
+	for(i = 0; i < mykeys.length; i++){
+		var x = mykeys[i];
+		if(x in otherWant){
+			if(otherWant[x] < myWant[x]){
+				var y = parseInt(otherWant[x]) + 1;
+        console.log(peers.peers[index].want[x]);
+				peers.peers[index].want[x] = y;
+        console.log(peers.peers[index].want[x]);
+				return x + ":" + y;
+			}
+
+		}
+		else{
+      peers.peers[index].want[x] = 0;
+      return x + ":" + 0
+    }
+	}
+
+	return false;
+
+
+}
+
+function i_want(){
+	var myWant = me.want;
+	for(j = 0; j<peers.peers.count; j++ ){
+		var otherWant = peers.peers[j].want;
+		var otherKeys = Object.keys(otherWant);
+		for(i = 0; i < otherKeys.length; i++){
+			var x = otherKeys[i];
+			if(x in myWant){
+				if(myWant[x] < otherWant[x]){
+					var y = myWant[x] + 1;
+					return x + ":" + y;
+				}
+
+			}
+			else{return x + ":" + 0}
+		}
+	}
+
+	return false;
+
+
+}
+
+function sendMsg(){
+
+	console.log("sendMsg");
+
+	if(Math.floor(Math.random() * 2) == 1){
+		//want
+
+    console.log("in want server");
+
+		var i = i_want();
+		if(i === false) i = Math.floor(Math.random() * peers.count);
+    console.log(i);
+    console.log(peers);
+		var u = JSON.stringify(peers.peers[i].url).replace(/\"/g, "");
+		var mu = u + "/message";
+		var full = {"Want": me.want, "Endpoint" : me.url};
+
+    console.log(full);
+		console.log(mu);
+
+		request.post(mu,{
+			headers: {'content-type' : 'application/json'},
+			form : {message : full}
+					}, function(error, response, body){
+						console.log(error);
+					  console.log(body);
+					});
+	}
+	else{
+		//rumor
+		for(i = 0; i < peers.count; i++){
+			var x = compareWants(peers.peers[i].want, i);
+			console.log(x);
+			if(x!=false){
+				for(j=0; j < messages.count; j++){
+					if(messages.messages[j].MessageID == x){
+						var u = peers.peers[i].url;
+						var mu = u + '/message';
+						var full = {"Rumor": messages.messages[j], "Endpoint":me.url};
+
+						console.log("in Rumor in server");
+            console.log(peers.peers[i]);
+						console.log(full);
+						request.post(mu,{
+										headers: {'content-type' : 'application/json'},
+									  form : {message : full}
+									}, function(error, response, body){
+										console.log(error);
+									  console.log(body);
+									});
+					}
+				}
+			}
+		}
+
+
+	}
+}
+
+
+function loop(){
+		//console.log("inloop");
+		if(peers.count > 0) sendMsg();
+		setTimeout(loop, 3000);
+}
